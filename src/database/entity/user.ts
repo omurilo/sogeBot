4ef673375758b0075afd689623d543e465e02942
sogehige <sogehige@gmail.com>
2022-08-05 10:33:32 +0200

    feat: remove followers permission (#5400)
    BREAKING CHANGE: followers permissions are not supported anymore.
    Instead consider use of viewers permission or subscriber permission.
    
    We are not tracking follower status anymore, every follower based permissions, etc.
    were deleted.

--- src/database/entity/user.ts
+++ src/database/entity/user.ts
@@ -59,13 +59,7 @@ export const User = new EntitySchema<Readonly<Required<UserInterface>>>({
     haveSubscribedAtLock: { type: Boolean, default: false },
     rank:                 { type: String, default: '' },
     haveCustomRank:       { type: Boolean, default: false },
-    followedAt:           {
-      type: 'varchar', length: '2022-07-27T00:30:34.569259834Z'.length, nullable: true,
-    },
-    followCheckAt: {
-      type: 'bigint', default: 0, transformer: new ColumnNumericTransformer(),
-    },
-    subscribedAt: {
+    subscribedAt:         {
       type: 'varchar', length: '2022-07-27T00:30:34.569259834Z'.length, nullable: true,
     },
     seenAt: {
